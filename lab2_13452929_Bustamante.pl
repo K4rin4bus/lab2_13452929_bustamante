7c55bd8fb993adc6afe8efff5b133f67c7a12243
K4rin4bus <karina.bustamante@usach.cl>
2023-05-18 00:07:03 -0400

    Avance, Requerimientos funcionales 4, 5 y 6

--- lab2_13452929_Bustamante.pl
+++ lab2_13452929_Bustamante.pl
@@ -1,18 +1,18 @@
 % TDA FileSystem - constructor (usa el timestamp, solo para el RF1)
-% Predicado: filesystem/4
+% Predicado: filesystem/6
 % Dominio: 
 % MetaPrincipal:filesystem(Nombre, Drives, Users, Content, [System])
 % MetaSecundaria:get_time(TimeStamp)
-filesystem(Nombre, Drives, Users, Content, [Nombre, Drives, Users, Content, TimeStamp]) :-
+filesystem(Nombre, Drives, Users, CurrentDrive, CurrentUser, Content, [Nombre, Drives, Users, CurrentDrive, CurrentUser, Content, TimeStamp]):-
    get_time(TimeStamp).
 
 
 % TDA FileSystem - constructor (para obtener el timestamp ya creado)
-% Predicado: filesystem/5
+% Predicado: filesystem/7
 % Dominio: 
 % MetaPrincipal:filesystem(Nombre, Drives, Users, Content, TimeStamp, [System])
 % MetaSecundaria:
-filesystem(Nombre, Drives, Users, Content, TimeStamp, [Nombre, Drives, Users, Content, TimeStamp]).
+filesystem(Nombre, Drives, Users, CurrentDrive, CurrentUser, Content, TimeStamp, [Nombre, Drives, Users, CurrentDrive, CurrentUser, Content, TimeStamp]).
 
 
 % TDA FileSystem - Modificador (modifica lista de drives en el sistema)
@@ -21,8 +21,8 @@ filesystem(Nombre, Drives, Users, Content, TimeStamp, [Nombre, Drives, Users, Co
 % MetaPrincipal:
 % MetaSecundaria:
 setDrives(System, UpdatedDrives, UpdatedSystem):-
-    filesystem(Nombre, _, Users, Content, TimeStamp, System),
-    filesystem(Nombre, UpdatedDrives, Users, Content, TimeStamp, UpdatedSystem).
+    filesystem(Nombre, _, Users, CurrentDrive, CurrentUser, Content, TimeStamp, System),
+    filesystem(Nombre, UpdatedDrives, Users, CurrentDrive, CurrentUser, Content, TimeStamp, UpdatedSystem).
 
 
 % TDA FileSystem - Modificador (modifica lista de users en el sistema)
@@ -31,11 +31,38 @@ setDrives(System, UpdatedDrives, UpdatedSystem):-
 % MetaPrincipal: setUsers(System, UpdatedUsers, UpdatedSystem)
 % MetaSecundaria: filesystem(Nombre, Drives, _, Content, TimeStamp, System),
 %               filesystem(Nombre, Drives, UpdatedUsers, Content, TimeStamp, UpdatedSystem).
-
 setUsers(System, UpdatedUsers, UpdatedSystem):-
-    filesystem(Nombre, Drives, _, Content, TimeStamp, System),
-    filesystem(Nombre, Drives, UpdatedUsers, Content, TimeStamp, UpdatedSystem).
+    filesystem(Nombre, Drives, _, CurrentDrive, CurrentUser, Content, TimeStamp, System),
+    filesystem(Nombre, Drives, UpdatedUsers, CurrentDrive, CurrentUser, Content, TimeStamp, UpdatedSystem).
 
+% TDA FileSystem - Modificador (modifica CurrentUser, logea usuario en sistema)
+% Predicado: setLoginUserInSystem/3
+% Dominio: 
+% MetaPrincipal: setLoginUserInSystem(System, User, UpdatedSystem
+% MetaSecundaria: filesystem(Nombre, Drives, Users, CurrentDrive, _, Content, TimeStamp, System),
+%               filesystem(Nombre, Drives, Users, CurrentDrive, User, Content, TimeStamp, UpdatedSystem)
+setLoginUser(System, User, UpdatedSystem):-
+    filesystem(Nombre, Drives, Users, CurrentDrive, _, Content, TimeStamp, System),
+    filesystem(Nombre, Drives, Users, CurrentDrive, User, Content, TimeStamp, UpdatedSystem).
+
+% TDA FileSystem - Modificador (modifica CurrentUser, deslogea usuario en sistema)
+% Predicado: setLogoutUser/2
+% Dominio: 
+% MetaPrincipal: setLogoutUser(System, UpdatedSystem)
+% MetaSecundaria: filesystem(Nombre, Drives, Users, CurrentDrive, _, Content, TimeStamp, System),
+%               filesystem(Nombre, Drives, Users, CurrentDrive, " ", Content, TimeStamp, UpdatedSystem).
+setLogoutUser(System, UpdatedSystem):-
+    filesystem(Nombre, Drives, Users, CurrentDrive, _, Content, TimeStamp, System),
+    filesystem(Nombre, Drives, Users, CurrentDrive, " ", Content, TimeStamp, UpdatedSystem).
+
+% TDA FileSystem - Modificador (modifica CurrentDrive en sistema)
+% Predicado: setCurrentDrive/3
+% Dominio: 
+% MetaPrincipal: setCurrentDrive(System, Unidad, UpdatedSystem)
+% MetaSecundaria:
+setCurrentDrive(System, Unidad, UpdatedSystem):-
+    filesystem(Nombre, Drives, Users, _, CurrentUser, Content, TimeStamp, System),
+    filesystem(Nombre, Drives, Users, Unidad, CurrentUser, Content, TimeStamp, UpdatedSystem).
 
 %========================================================
 % TDA Drive - Constructor
@@ -50,37 +77,55 @@ drive(Unidad, Nombre, Capacidad,[Unidad, Nombre, Capacidad]).
 % MetaPrincipal: getDrives(System, Drives)
 % MetaSecundaria: filesystem(_, Drives, _, _, System).
 getDrives(System, Drives):-
-    filesystem(_, Drives, _, _, _, System).
+    filesystem(_, Drives, _, _, _, _, _, System).
+
+%no existe unidad en sistema
+notLetterDriveInSystem(Unidad, System) :-
+   filesystem(_, Drives, _, _, _, _, _, System),
+    \+ exists(Unidad, Drives). %  \+ es not
+
+%existe unidad en sistema
+letterDriveInSystem(Unidad, System) :-
+    filesystem(_, Drives, _, _, _, _, _, System),
+    exists(Unidad, Drives).
+
+% TDA  - Pertenencia (revisa si existe elemento en lista)
+% Predicado: exists/2
+% Dominio: 
+% MetaPrincipal: 
+% MetaSecundaria:
+exists(Elemento, [Cabecera|_]) :- % Base caso, primer elemento de lista
+   member(Elemento, Cabecera).
+
+exists(Elemento, [_|RestoListas]) :- % Recursivo caso
+   exists(Elemento, RestoListas).
+
 
 % TDA Drive - Modificador (agrega drive a lista de drives del sistema)
 % Predicado: setAddNewDriveInDrives/3
 % Dominio: 
 % MetaPrincipal: setAddNewDriveInDrives(NewDrive, Drives, UpdatedDrives)
 % MetaSecundaria:  append(Drives, [NewDrive], UpdatedDrives)
-setAddNewDriveInDrives(NewDrive, Drives, UpdatedDrives) :-
+setAddNewDriveInDrives(NewDrive, Drives, UpdatedDrives):-
    append(Drives, [NewDrive], UpdatedDrives).
 
-
 %========================================================
-
-% TDA User - Modificador
-% Predicado: setAddUserInSystem/3
-% Dominio: 
-% MetaPrincipal: setAddUserInSystem(System, User, UpdatedSystem)
-% MetaSecundaria: getUsers(System, Users), setAddUserInUsers(Users, User, UpdatedUsers),
-%                 setUsers(System, UpdatedUsers, UpdatedSystem)
-setAddUserInSystem(System, User, UpdatedSystem):-
-    getUsers(System, Users),
-    setAddUserInUsers(Users, User, UpdatedUsers),
-    setUsers(System, UpdatedUsers, UpdatedSystem).
-
 % TDA User - Selector
 % Predicado: getUsers/2
 % Dominio: 
 % MetaPrincipal: getUsers(System, Users)
 % MetaSecundaria: filesystem(_, _, Users, _, _, System)
 getUsers(System, Users):-
-    filesystem(_, _, Users, _, _, System).
+    filesystem(_, _, Users, _, _, _, _, System).
+
+
+% no pertenece
+notExistsUser(User, Users) :-
+   \+ member(User, Users).
+
+% pertenece
+existsUser(User, Users) :-
+   member(User, Users).
 
 % TDA User - Modificador
 % Predicado: setAddUserInUsers/3
@@ -90,7 +135,18 @@ getUsers(System, Users):-
 setAddUserInUsers(Users, User, UpdatedUsers):-
     append(Users, [User], UpdatedUsers).
 
-
+%========================================================
+% TDA User - Selector
+% Predicado: getCurrentUser/2
+% Dominio: 
+% MetaPrincipal: getCurrentUser(System, CurrentUser)
+% MetaSecundaria: filesystem(_, _, _, _, CurrentUser, _, _, System)
+getCurrentUser(System, CurrentUser):-
+    filesystem(_, _, _, _, CurrentUser, _, _, System).
+    
+existeCurrentUser(CurrentUser):-
+    CurrentUser \== "".
+    
 %========================================================
 %   Reglas requerimientos funcionales
 %========================================================
@@ -98,15 +154,15 @@ setAddUserInUsers(Users, User, UpdatedUsers):-
 %RF-1 System
 % creando un nuevo sistema con nombre “NewSystem”
 % system("NewSystem", S).
-system(Nombre, Sistema) :-
-   filesystem(Nombre, [], [], [], Sistema).
+system(Nombre, Sistema):-
+   filesystem(Nombre, [], [], " ", " ", [], Sistema).
 
 %RF-2 systemAddDrive
-% creando la unidad C, con nombre "OS" y capacidad 1000000000 en el sistema "NewSystem"
+% creando la unidad C, con nombre “OS” y capacidad 1000000000 en el sistema “NewSystem”
 % system("NewSystem", S),
 % systemAddDrive(S, "C",  "OS", 10000000000, S2).
-systemAddDrive(System, Unidad, Nombre, Capacidad, UpdatedSystem) :-
-    %letterDoesntExistsInSystem(Unidad, System), % Verifico que no existe un drive con la misma letra
+systemAddDrive(System, Unidad, Nombre, Capacidad, UpdatedSystem):-
+    notLetterDriveInSystem(Unidad, System), % Verifico que no existe un drive con la misma letra
     drive(Unidad, Nombre, Capacidad, NewDrive),
     getDrives(System, CurrentDrives),
     setAddNewDriveInDrives(NewDrive, CurrentDrives, UpdatedDrives),
@@ -118,6 +174,32 @@ systemAddDrive(System, Unidad, Nombre, Capacidad, UpdatedSystem) :-
 % systemAddDrive(S, "C",  "OS", 10000000000, S2),
 % systemRegister(S2, "user1", S3).
 systemRegister(System, User, UpdatedSystem):-
-    setAddUserInSystem(System, User, UpdatedSystem).
+    getUsers(System, Users),
+    notExistsUser(User, Users), 
+    setAddUserInUsers(Users, User, UpdatedUsers),
+    setUsers(System, UpdatedUsers, UpdatedSystem).
     
+%RF-4 systemLogin
+%iniciando sesión con "user1" siempre y cuando exista
+%systemLogin(S, "user1", S2).
+systemLogin(System, User, UpdatedSystem):-
+    getUsers(System, Users),
+    existsUser(User, Users),
+    setLoginUser(System, User, UpdatedSystem).
 
+%RF-5 systemLogout
+% se asume que S tiene sesión iniciada con “user1”
+%systemLogout(S, S2).
+systemLogout(System, UpdatedSystem):-
+    getCurrentUser(System, CurrentUser),
+    existeCurrentUser(CurrentUser),
+    setLogoutUser(System, UpdatedSystem).
+
+%RF-6 systemSwitchDrive
+%Cambiándose a la unidad C, debe existir usuario logeado
+%systemSwitchDrive(S, "C", S2).
+systemSwitchDrive(System, Unidad, UpdatedSystem):-
+    getCurrentUser(System, CurrentUser),
+    existeCurrentUser(CurrentUser),
+    letterDriveInSystem(Unidad, System),
+    setCurrentDrive(System, Unidad, UpdatedSystem).
---
+++ pruebas_13452929_Bustamante.pl
@@ -0,0 +1,9 @@
+system("NewSystem", S),
+systemAddDrive(S, "C",  "OS", 10000000000, S1),
+systemAddDrive(S1, "D",  "IOS", 20000000000, S2),
+systemRegister(S2, "user1", S3),
+systemRegister(S3, "user2", S4),
+systemLogin(S4, "user2", S5),
+systemLogout(S5, S6),
+systemLogin(S6, "user1", S7),
+systemSwitchDrive(S7, "C", S8).
